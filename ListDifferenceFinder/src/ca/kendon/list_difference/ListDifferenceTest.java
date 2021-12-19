dd6c8af7fb7fca33845319f613b3c42a6b309b80
Tyson Kendon <tj@kendon.ca>
2021-12-18 19:06:48 -0700

    Added tests for only in A / B

--- ListDifferenceFinder/src/ca/kendon/list_difference/ListDifferenceTest.java
+++ ListDifferenceFinder/src/ca/kendon/list_difference/ListDifferenceTest.java
@@ -1,4 +1,111 @@
-import static org.junit.jupiter.api.Assertions.*;
+package ca.kendon.list_difference;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 class ListDifferenceTest {
-  
+
+    @Test
+    void onlyInA() {
+        ArrayList<String> a = new ArrayList<>();
+        a.add("A");
+        a.add("B");
+        a.add("C");
+        ArrayList<String> b= new ArrayList<>();
+        b.add("B");
+        b.add("C");
+
+        ListDifference diff = new ListDifference(a, b);
+
+        Set<String> expected = new HashSet<>();
+        expected.add("A");
+
+        Set<String> result = (HashSet<String>) diff.onlyInA();
+        Assertions.assertIterableEquals(expected, result);
+
+    }
+
+    @Test
+    void onlyInA_noneOnlyInA() {
+        ArrayList<String> a = new ArrayList<>();
+        a.add("B");
+        a.add("C");
+        ArrayList<String> b= new ArrayList<>();
+        b.add("A");
+        b.add("B");
+        b.add("C");
+
+        ListDifference diff = new ListDifference(a, b);
+
+        Set<String> expected = new HashSet<>();
+
+        Set<String> result = (HashSet<String>) diff.onlyInA();
+        Assertions.assertIterableEquals(expected, result);
+
+    }
+
+    @Test
+    void onlyInB_noneOnlyInB() {
+        ArrayList<String> a = new ArrayList<>();
+        a.add("A");
+        a.add("B");
+        a.add("C");
+        ArrayList<String> b= new ArrayList<>();
+        b.add("B");
+        b.add("C");
+
+        ListDifference diff = new ListDifference(a, b);
+
+        Set<String> expected = new HashSet<>();
+
+        Set<String> result = (HashSet<String>) diff.onlyInB();
+        Assertions.assertIterableEquals(expected, result);
+
+    }
+
+    @Test
+    void onlyInB() {
+
+        ArrayList<String> a = new ArrayList<>();
+        a.add("B");
+        a.add("C");
+        ArrayList<String> b= new ArrayList<>();
+        b.add("A");
+        b.add("B");
+        b.add("C");
+
+        ListDifference diff = new ListDifference(a, b);
+
+        Set<String> expected = new HashSet<>();
+        expected.add("A");
+
+        Set<String> result = (HashSet<String>) diff.onlyInB();
+        Assertions.assertIterableEquals(expected, result);
+    }
+
+    @Test
+    void union() {
+    }
+
+    @Test
+    void percentInA() {
+    }
+
+    @Test
+    void percentInB() {
+    }
+
+    @Test
+    void intersection() {
+    }
+
+    @Test
+    void similarity() {
+    }
 }
