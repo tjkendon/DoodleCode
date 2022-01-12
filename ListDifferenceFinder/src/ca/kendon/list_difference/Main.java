9fcdac19d8ee07f309994c10bbffc003e7941b4a
Tyson Kendon <tj@kendon.ca>
2022-01-11 21:51:15 -0700

    Started building cl system

--- ListDifferenceFinder/src/ca/kendon/list_difference/Main.java
+++ ListDifferenceFinder/src/ca/kendon/list_difference/Main.java
@@ -6,7 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
 
+    }
 
+    public static void printAll() {
         File fileA = new File("data/testA");
         File fileB = new File("data/testB");
         ListDifferenceEngine engine = new ListDifferenceEngine(fileA, fileB);
@@ -23,7 +25,112 @@ public class Main {
         System.out.printf("SD(A, B): %s%n", difference.sdSimilarity());
         System.out.printf("O(A, B): %s%n", difference.overlapSimilarity());
 
+    }
 
+    public static String generateString() {
+        StringBuilder builder = new StringBuilder();
+        return builder.toString();
+    }
 
+    private static class outputControl {
+        private boolean showA = false;
+        private boolean showB = false;
+        private boolean showUnion = false;
+        private boolean showIntersection = false;
+        private boolean showUniqueA = false;
+        private boolean showUniqueB = false;
+        private boolean showAPercent = false;
+        private boolean showBPercent = false;
+        private boolean showJaccard = false;
+        private boolean showSD = false;
+        private boolean showOverlap = false;
+
+        public boolean isShowA() {
+            return showA;
+        }
+
+        public void setShowA(boolean showA) {
+            this.showA = showA;
+        }
+
+        public boolean isShowB() {
+            return showB;
+        }
+
+        public void setShowB(boolean showB) {
+            this.showB = showB;
+        }
+
+        public boolean isShowUnion() {
+            return showUnion;
+        }
+
+        public void setShowUnion(boolean showUnion) {
+            this.showUnion = showUnion;
+        }
+
+        public boolean isShowIntersection() {
+            return showIntersection;
+        }
+
+        public void setShowIntersection(boolean showIntersection) {
+            this.showIntersection = showIntersection;
+        }
+
+        public boolean isShowUniqueA() {
+            return showUniqueA;
+        }
+
+        public void setShowUniqueA(boolean showUniqueA) {
+            this.showUniqueA = showUniqueA;
+        }
+
+        public boolean isShowUniqueB() {
+            return showUniqueB;
+        }
+
+        public void setShowUniqueB(boolean showUniqueB) {
+            this.showUniqueB = showUniqueB;
+        }
+
+        public boolean isShowAPercent() {
+            return showAPercent;
+        }
+
+        public void setShowAPercent(boolean showAPercent) {
+            this.showAPercent = showAPercent;
+        }
+
+        public boolean isShowBPercent() {
+            return showBPercent;
+        }
+
+        public void setShowBPercent(boolean showBPercent) {
+            this.showBPercent = showBPercent;
+        }
+
+        public boolean isShowJaccard() {
+            return showJaccard;
+        }
+
+        public void setShowJaccard(boolean showJaccard) {
+            this.showJaccard = showJaccard;
+        }
+
+        public boolean isShowSD() {
+            return showSD;
+        }
+
+        public void setShowSD(boolean showSD) {
+            this.showSD = showSD;
+        }
+
+        public boolean isShowOverlap() {
+            return showOverlap;
+        }
+
+        public void setShowOverlap(boolean showOverlap) {
+            this.showOverlap = showOverlap;
+        }
     }
 }
