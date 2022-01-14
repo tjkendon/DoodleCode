02e141a383bdd5b4dfde111832ef2f5689e0faa1
Tyson Kendon <tj@kendon.ca>
2022-01-14 15:40:39 -0700

    Built simple command line system

--- ListDifferenceFinder/src/ca/kendon/list_difference/Main.java
+++ ListDifferenceFinder/src/ca/kendon/list_difference/Main.java
@@ -1,38 +1,178 @@
 package ca.kendon.list_difference;
 
+import org.apache.commons.cli.*;
+
 import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
 
     public static void main(String[] args) {
 
-    }
-
-    public static void printAll() {
-        File fileA = new File("data/testA");
-        File fileB = new File("data/testB");
-        ListDifferenceEngine engine = new ListDifferenceEngine(fileA, fileB);
-        ListDifference difference = engine.getListDifference();
-        System.out.printf("A: %s%n", difference.getA());
-        System.out.printf("B: %s%n", difference.getB());
-        System.out.printf("A U B: %s%n", difference.union());
-        System.out.printf("/A: %s%n", difference.onlyInA());
-        System.out.printf("%%/A: %s%n", difference.percentInA());
-        System.out.printf("/B: %s%n", difference.onlyInB());
-        System.out.printf("%%/B: %s%n",difference.percentInB());
-        System.out.printf("A^ B: %s%n", difference.intersection());
-        System.out.printf("Jaccard(A, B): %s%n", difference.jaccardSimilarity());
-        System.out.printf("SD(A, B): %s%n", difference.sdSimilarity());
-        System.out.printf("O(A, B): %s%n", difference.overlapSimilarity());
+        OutputControl control = generateControl(args);
+        ListDifferenceEngine engine = new ListDifferenceEngine(control.getFileA(), control.getFileB());
+        System.out.println(generateOutputString(control, engine.getListDifference()));
 
     }
 
-    public static String generateString() {
+
+    public static OutputControl generateControl(String[] args) {
+        Options options = new Options();
+        options.addOption(
+                "a",
+                "showA",
+                false,
+                "show the elements in a");
+        options.addOption(
+                "b",
+                "showB",
+                false,
+                "show the elements in b");
+        options.addOption(
+                "u",
+                "showUnion",
+                false,
+                "show the elements in the union");
+        options.addOption(
+                "i",
+                "showIntersection",
+                false,
+                "show the elements in the intersection");
+        options.addOption(
+                "A",
+                "uniqueA",
+                false,
+                "show the unique elements in A");
+        options.addOption(
+                "B",
+                "uniqueB",
+                false,
+                "show the unique elements in B");
+        options.addOption(
+                "p",
+                "percentA",
+                false,
+                "show the percentage of unique elements in A");
+        options.addOption(
+                "a",
+                "percentB",
+                false,
+                "show the percentage of unique elements in B");
+        options.addOption(
+                "j",
+                "showJaccard",
+                false,
+                "show the Jaccard similarity between A and B");
+        options.addOption(
+                "s",
+                "showSD",
+                false,
+                "show the Soerensen-Dice similarity between A and B");
+        options.addOption(
+                "o",
+                "showO",
+                false,
+                "show the Szymkiewicz–Simpson similarity between A and B");
+
+        OutputControl control = new OutputControl();
+
+        CommandLineParser parser = new DefaultParser();
+
+        try {
+            CommandLine cl = parser.parse(options, args);
+
+            if (cl.hasOption("showA")) {
+                control.setShowA(true);
+            }
+            if (cl.hasOption("showB")) {
+                control.setShowB(true);
+            }
+            if (cl.hasOption("showUnion")) {
+                control.setShowUnion(true);
+            }
+            if (cl.hasOption("showIntersection")) {
+                control.setShowIntersection(true);
+            }
+            if (cl.hasOption("uniqueA")) {
+                control.setShowUniqueA(true);
+            }
+            if (cl.hasOption("uniqueB")) {
+                control.setShowUniqueB(true);
+            }
+            if (cl.hasOption("percentA")) {
+                control.setShowAPercent(true);
+            }
+            if (cl.hasOption("percentB")) {
+                control.setShowBPercent(true);
+            }
+            if (cl.hasOption("showJaccard")) {
+                control.setShowJaccard(true);
+            }
+            if (cl.hasOption("showSD")) {
+                control.setShowSD(true);
+            }
+            if (cl.hasOption("showO")) {
+                control.setShowOverlap(true);
+            }
+            if (args.length > 2) {
+                control.setFileA(new File(args[args.length - 2]));
+                control.setFileB(new File(args[args.length - 1]));
+            } else {
+                HelpFormatter formatter = new HelpFormatter();
+                formatter.printHelp("ListDifference <options> fileA fileB", options);
+            }
+        } catch (ParseException e) {
+            Logger.getLogger("ListDifference").log(
+                    Level.WARNING, String.format("Error parsing command line %s", e.getLocalizedMessage()));
+
+        }
+        
+        return control;
+
+    }
+
+    public static String generateOutputString(OutputControl control, ListDifference difference) {
         StringBuilder builder = new StringBuilder();
+
+        if (control.isShowA()) {
+            builder.append(String.format("A: %s%n", difference.getA()));
+        }
+        if (control.isShowUniqueA()) {
+            builder.append(String.format("/A: %s%n", difference.onlyInA()));
+        }
+        if (control.isShowAPercent()) {
+            builder.append(String.format("%%/A: %s%n", difference.percentInA()));
+        }
+        if (control.isShowB()) {
+            builder.append(String.format("B: %s%n", difference.getB()));
+        }
+        if (control.isShowUniqueB()) {
+            builder.append(String.format("/B: %s%n", difference.onlyInB()));
+        }
+        if (control.isShowBPercent()) {
+            builder.append(String.format("%%/B: %s%n", difference.percentInB()));
+        }
+        if (control.isShowUnion()) {
+            builder.append(String.format("A U B: %s%n", difference.union()));
+        }
+        if (control.isShowIntersection()) {
+            builder.append(String.format("A ^ B: %s%n", difference.intersection()));
+        }
+        if (control.isShowJaccard()) {
+            builder.append(String.format("Jaccard(A, B): %s%n", difference.jaccardSimilarity()));
+        }
+        if (control.isShowSD()) {
+            builder.append(String.format("SD(A, B): %s%n", difference.sdSimilarity()));
+        }
+        if (control.isShowOverlap()) {
+            builder.append(String.format("O(A, B): %s%n", difference.overlapSimilarity()));
+        }
+
         return builder.toString();
     }
 
-    private static class outputControl {
+    private static class OutputControl {
         private boolean showA = false;
         private boolean showB = false;
         private boolean showUnion = false;
@@ -44,6 +184,18 @@ public class Main {
         private boolean showJaccard = false;
         private boolean showSD = false;
         private boolean showOverlap = false;
+
+        private File fileA;
+        private File fileB;
+
+
+        public File getFileA() {
+            return fileA;
+        }
+
+        public File getFileB() {
+            return fileB;
+        }
 
         public boolean isShowA() {
             return showA;
@@ -131,6 +283,14 @@ public class Main {
 
         public void setShowOverlap(boolean showOverlap) {
             this.showOverlap = showOverlap;
+        }
+
+        public void setFileA(File fileA) {
+            this.fileA = fileA;
+        }
+
+        public void setFileB(File fileB) {
+            this.fileB = fileB;
         }
     }
 }
