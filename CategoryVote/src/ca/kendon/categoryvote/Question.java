ccb55c16cac6f94bad4b232b77832ace16d6aa0c
Tyson Kendon <tj@kendon.ca>
2022-02-09 10:43:58 -0700

    Started building out question

--- CategoryVote/src/ca/kendon/categoryvote/Question.java
+++ CategoryVote/src/ca/kendon/categoryvote/Question.java
@@ -4,8 +4,25 @@ import java.util.List;
 
 public class Question {
 
+    private String name;
     private String stem;
     private List<Option> options;
 
+    public Question(String name, String stem, List<Option> options) {
+        this.name = name;
+        this.stem = stem;
+        this.options = options;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getStem() {
+        return stem;
+    }
+
+    public List<Option> getOptions() {
+        return options;
+    }
 }
