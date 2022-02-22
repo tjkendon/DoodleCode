cb4fae19ca9d5352c79401da663c4abcacac5b2f
Tyson Kendon <tj@kendon.ca>
2022-02-21 19:49:00 -0700

    Added equals and hashcodes for Questions and Options

--- CategoryVote/src/ca/kendon/categoryvote/Option.java
+++ CategoryVote/src/ca/kendon/categoryvote/Option.java
@@ -21,4 +21,22 @@ public class Option {
         return new HashMap<>(pointAssignment);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Option option = (Option) o;
+
+        if (text != null ? !text.equals(option.text) : option.text != null) return false;
+        return pointAssignment != null ?
+                pointAssignment.equals(option.pointAssignment) : option.pointAssignment == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = text != null ? text.hashCode() : 0;
+        result = 31 * result + (pointAssignment != null ? pointAssignment.hashCode() : 0);
+        return result;
+    }
 }
--- CategoryVote/src/ca/kendon/categoryvote/Question.java
+++ CategoryVote/src/ca/kendon/categoryvote/Question.java
@@ -27,4 +27,23 @@ public class Question {
         return options;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Question question = (Question) o;
+
+        if (name != null ? !name.equals(question.name) : question.name != null) return false;
+        if (stem != null ? !stem.equals(question.stem) : question.stem != null) return false;
+        return options != null ? options.equals(question.options) : question.options == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (stem != null ? stem.hashCode() : 0);
+        result = 31 * result + (options != null ? options.hashCode() : 0);
+        return result;
+    }
 }
