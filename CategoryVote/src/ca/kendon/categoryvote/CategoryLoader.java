ec67b275d1916a2d8e73da257c517ad29ccff7b6
Tyson Kendon <tj@kendon.ca>
2022-02-21 18:09:54 -0700

    Fixed trimming to handle leading spaces in cateogry items

--- CategoryVote/src/ca/kendon/categoryvote/CategoryLoader.java
+++ CategoryVote/src/ca/kendon/categoryvote/CategoryLoader.java
@@ -3,6 +3,7 @@ package ca.kendon.categoryvote;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 public class CategoryLoader {
 
@@ -11,10 +12,15 @@ public class CategoryLoader {
     public static Category load(String categoryString) {
 
         List<String> categoryData = new ArrayList(Arrays.asList(categoryString.split(CATEGORY_FIELD_SEPARATOR)));
+
         String name = categoryData.remove(0);
         int points = Integer.parseInt(categoryData.remove(0).trim());
+        ArrayList<String> ccd = new ArrayList<>();
+        for (String s : categoryData) {
+            ccd.add(s.trim());
+        }
 
-        return new Category(name, points, categoryData);
+        return new Category(name, points, ccd);
 
     }
 }
