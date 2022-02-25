1f509d5a1825bf96771690f2b2f853070a445168
Tyson Kendon <tj@kendon.ca>
2022-02-25 10:41:17 -0700

    Added tool to let users answer questions.
    Probably too much in one commit, but brought in the category and questions and built a loop to show the questions and options.

---
+++ CategoryVote/src/ca/kendon/categoryvote/CommandLineQuestionAsker.java
@@ -0,0 +1,77 @@
+package ca.kendon.categoryvote;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class CommandLineQuestionAsker {
+
+    public static void main(String[] args) {
+
+
+        File categoryFile = new File(args[0]);
+        File questionFile = new File(args[1]);
+        File responseFile = new File(args[2]);
+
+        try {
+            CommandLineQuestionAsker asker = new CommandLineQuestionAsker(categoryFile, questionFile, responseFile);
+            asker.run();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private List<Category> categories;
+    private List<Question> questions;
+
+    private File responseFile;
+
+    public CommandLineQuestionAsker(File categoryFile, File questionFile, File responseFile) throws FileNotFoundException {
+
+            categories = CategoryLoader.readFile(categoryFile);
+            questions = QuestionLoader.readFile(questionFile, categories);
+
+
+        this.responseFile = responseFile;
+
+
+    }
+
+    public void run() throws FileNotFoundException {
+
+        List<Response> responses = askQuestions(questions);
+
+        ResponseLoader.writeFile(responseFile, responses);
+    }
+
+    private List<Response> askQuestions(List<Question> questions) {
+
+        Scanner reader = new Scanner(System.in);
+
+        ArrayList<Response> responses = new ArrayList<>();
+
+        System.out.println("Please answer the following questions:");
+        int questionNumber = 1;
+        for (Question q : questions) {
+            System.out.println("Question " + questionNumber++ + ": " + q.getName());
+            System.out.println(q.getStem());
+            int optionNumber = 1;
+            for (Option o : q.getOptions()) {
+                System.out.println(optionNumber++ + ") " + o.getText());
+            }
+            System.out.print("Choice: ");
+            int choice = reader.nextInt();
+            System.out.println("Picked " + q.getOptions().get(choice - 1));
+            System.out.println();
+        }
+
+        return responses;
+
+    }
+
+
+}
