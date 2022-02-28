a1c11ce873a1a120a16850173b0e2f94ce113f08
Tyson Kendon <tj@kendon.ca>
2022-02-27 17:47:44 -0700

    Added command line result calculator

---
+++ CategoryVote/src/ca/kendon/categoryvote/CommandLineResultCalculator.java
@@ -0,0 +1,47 @@
+package ca.kendon.categoryvote;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommandLineResultCalculator {
+
+
+    private List<Category> categories;
+    private List<Question> questions;
+    private List<Response> responses;
+
+    public static void main(String[] args) {
+
+        try {
+            CommandLineResultCalculator calculator = new CommandLineResultCalculator(
+                    new File(args[0]),
+                    new File(args[1]),
+                    new File(args[2]));
+            List<String> results = calculator.run(Integer.parseInt(args[3]));
+
+            System.out.println(results);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public CommandLineResultCalculator(File categoryFile, File questionFile, File responseFile)
+            throws FileNotFoundException {
+
+        categories = CategoryLoader.readFile(categoryFile);
+        questions = QuestionLoader.readFile(questionFile, categories);
+        responses = ResponseLoader.readFile(responseFile, questions);
+
+    }
+
+    public List<String> run(int winners) {
+        VotingEngine engine = new VotingEngine(categories);
+        return engine.evaluate(responses, 4);
+
+    }
+
+}
