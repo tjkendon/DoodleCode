270d3f051d89930b1b2d7b3080f6d41be7db8a30
Tyson Kendon <tj@kendon.ca>
2022-03-11 15:09:34 -0700

    Clean up

--- CategoryVote/src/ca/kendon/categoryvote/Category.java
+++ CategoryVote/src/ca/kendon/categoryvote/Category.java
@@ -1,9 +1,16 @@
 package ca.kendon.categoryvote;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
+import java.util.List;
+import java.util.Objects;
+
+/**
+ *
+ * A Category, including a name, the number of points the category is reduced by each time it wins,
+ * and a possible list of which are "won" when the category wins
+ *
+ */
 public class Category {
 
     final private String name;
@@ -12,26 +19,65 @@ public class Category {
 
     final List<String> victoryItems;
 
+    /**
+     *
+     * Constructs a new Category with name and victory point reduction and an empty list of
+     * victory items.
+     *
+     * @param name the category name, used as identifier
+     * @param victoryPointReduction the number of points the total is reduced by while determining vote results for a
+     *                              win for this category
+     */
     public Category(String name, int victoryPointReduction) {
         this.name = name;
         this.victoryPointReduction = victoryPointReduction;
         this.victoryItems = new ArrayList<>();
     }
 
+    /**
+     *
+     * Constructs a new Category with name and victory point reduction and a list of
+     * victory items.
+     *
+     * @param name the category name, used as identifier
+     * @param victoryPointReduction the number of points the total is reduced by while determining vote results for a
+     *                              win for this category
+
+     * @param victoryItems list of items which are returned in sequence when this category wins a vote
+     */
     public Category(String name, int victoryPointReduction, List<String> victoryItems) {
         this.name = name;
         this.victoryPointReduction = victoryPointReduction;
         this.victoryItems = victoryItems;
     }
 
+    /**
+     *
+     * Returns the name of the category, which should be an identifier
+     *
+     * @return the category name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     *
+     * Returns the the number of points the total is reduced by while determining vote results for a
+     * win for this category
+     *
+     * @return number of points
+     */
     public int getVictoryPointReduction() {
         return victoryPointReduction;
     }
 
+    /**
+     *
+     * The list of items which can be won when this category wins a voting round.
+     *
+     * @return the list of items
+     */
     public List<String> getVictoryItems() {
         return victoryItems;
     }
@@ -49,8 +95,8 @@ public class Category {
         Category category = (Category) o;
 
         if (victoryPointReduction != category.victoryPointReduction) return false;
-        if (name != null ? !name.equals(category.name) : category.name != null) return false;
-        return victoryItems != null ? victoryItems.equals(category.victoryItems) : category.victoryItems == null;
+        if (!Objects.equals(name, category.name)) return false;
+        return Objects.equals(victoryItems, category.victoryItems);
     }
 
     @Override
