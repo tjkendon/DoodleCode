49528366bb90b6b2e483c2e185bb44325345a1ac
Tyson Kendon <tj@kendon.ca>
2022-03-12 15:55:44 -0700

    Added 3d distance calculator for colour

--- ColourDistance/colour_distance.py
+++ ColourDistance/colour_distance.py
@@ -1,7 +1,12 @@
+import math
 
 def colour_value ( name ):
 	"Split name into 3 hex tuples"
-	return (int(colour_name_1[0:2], 16), int(colour_name_1[2:4], 16), int(colour_name_1[4:6], 16))
+	return (int(name[0:2], 16), int(name[2:4], 16), int(name[4:6], 16))
+
+def colour_distance ( colour_1, colour_2 ):
+	"Find the distance between these 2 colours (as points)"
+	return math.sqrt( (colour_2[0]-colour_1[0])*(colour_2[0]-colour_1[0]) + (colour_2[1]-colour_1[1])*(colour_2[1]-colour_1[1]) + (colour_2[2]-colour_1[2])*(colour_2[2]-colour_1[2]) )
 
 colour_name_1 = input("Colour 1: ")
 colour_name_2 = input("Colour 2: ")
@@ -9,4 +14,6 @@ colour_name_2 = input("Colour 2: ")
 colour_1 = colour_value( colour_name_1 )
 colour_2 = colour_value( colour_name_2 )
 
-print(f'Colour 1: {colour_1} Colour 2: {colour_2}')
+distance = colour_distance(colour_1, colour_2)
+
+print(f'Colour 1: {colour_1} Colour 2: {colour_2} Distance: {distance:.3f}')
