6f1dac951728dec1ecd0c593e2009242b4b14758
Tyson Kendon <tj@kendon.ca>
2022-03-12 14:53:53 -0700

    Breaking colour string

--- ColourDistance/colour_distance.py
+++ ColourDistance/colour_distance.py
@@ -3,4 +3,5 @@
 colour_name_1 = input("Colour 1: ")
 colour_name_2 = input("Colour 2: ")
 
-print("Colours are ", colour_name_1, " ", colour_name_2)
+print("Colours are ", colour_name_1, colour_name_2)
+print("Colour 1: ", colour_name_1[0:2], colour_name_1[2:4], colour_name_1[4:6])
