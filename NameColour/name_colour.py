5074ebfdd38c92159691c0d92d72f60e0b2e9789
Tyson Kendon <tj@kendon.ca>
2022-03-16 23:15:34 -0600

    Built basic colour generator based on string

--- NameColour/name_colour.py
+++ NameColour/name_colour.py
@@ -1,5 +1,15 @@
 
 
+def hex_str(colour):
+        return ''.join('{:02X}'.format(a) for a in colour)
+
 
 name = input("Enter Name: ")
+ident = 1 
 print(name)
+for c in name:
+	ident *= ord(c)
+r = int((ident / 31) % 255)
+b = int((ident / 7) % 255)
+g = int((ident / 255) % 255)
+print(hex_str((r, g, b)))
