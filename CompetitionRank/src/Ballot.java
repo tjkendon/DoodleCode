15b98e62a9705d0063442c424d574220d64e6770
Tyson Kendon <tj@kendon.ca>
2022-04-13 10:00:40 -0600

    Added constructor to take list of options

--- CompetitionRank/src/Ballot.java
+++ CompetitionRank/src/Ballot.java
@@ -1,2 +1,18 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Ballot {
+
+    private Map<Option, Integer> options;
+
+    public Ballot (List<Option> options) {
+        this.options = new HashMap<>();
+        for (Option o : options) {
+            this.options.put(o, 0);
+        }
+    }
+
+
+
 }
