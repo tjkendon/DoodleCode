1b3b42f9fc0c930a4476f1a3c41663bb612c2876
Tyson Kendon <tj@kendon.ca>
2022-05-05 10:25:05 -0600

    Added reader for competition round as strings

--- CompetitionRank/src/ca/kendon/competitionrank/CompetitionRoundReader.java
+++ CompetitionRank/src/ca/kendon/competitionrank/CompetitionRoundReader.java
@@ -1,2 +1,19 @@
-package ca.kendon.competitionrank;public class CompetitionRoundReader {
+package ca.kendon.competitionrank;
+
+public class CompetitionRoundReader {
+
+
+    public CompetitionRound read(String roundData) {
+
+        CompetitionRound round = new CompetitionRound();
+
+
+        String[] data = roundData.split(",");
+        for (int i = 0; i < data.length; i += 2) {
+            round.putOption(new Option(data[i].trim()), Integer.parseInt(data[i+1].trim()));
+        }
+
+        return round;
+    }
+
 }
