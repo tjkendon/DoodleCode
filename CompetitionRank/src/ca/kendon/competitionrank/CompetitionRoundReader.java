b3b9cb8645a8467a16ebfa2b486ce85f59a06b3b
Tyson Kendon <tj@kendon.ca>
2022-05-05 10:33:59 -0600

    Added code to handle named or unnamed rounds

--- CompetitionRank/src/ca/kendon/competitionrank/CompetitionRoundReader.java
+++ CompetitionRank/src/ca/kendon/competitionrank/CompetitionRoundReader.java
@@ -5,11 +5,19 @@ public class CompetitionRoundReader {
 
     public CompetitionRound read(String roundData) {
 
-        CompetitionRound round = new CompetitionRound();
 
-
+        String name = "Read";
         String[] data = roundData.split(",");
-        for (int i = 0; i < data.length; i += 2) {
+        int firstEntry = 0;
+
+        if (data.length % 2 != 0) {
+            name = data[0];
+            firstEntry = 1;
+        }
+
+        CompetitionRound round = new CompetitionRound(name);
+
+        for (int i = firstEntry; i < data.length; i += 2) {
             round.putOption(new Option(data[i].trim()), Integer.parseInt(data[i+1].trim()));
         }
 
