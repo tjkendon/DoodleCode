ade9033011b70506f216c43bea3565656f349c83
Tyson Kendon <tj@kendon.ca>
2022-05-08 17:55:11 -0600

    ../src/ca/kendon/competitionrank/Ranking.java

--- CompetitionRank/src/ca/kendon/competitionrank/Ranking.java
+++ CompetitionRank/src/ca/kendon/competitionrank/Ranking.java
@@ -1,2 +1,29 @@
-package ca.kendon.competitionrank;public class Ranking {
+package ca.kendon.competitionrank;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Ranking {
+
+    public void generateRanking(List<CompetitionRound> rounds) {
+
+        Map<Option, OptionData> data = new HashMap<>();
+
+        for (CompetitionRound r : rounds) {
+            for (Option o : r.getOptions()) {
+                if (!data.containsKey(o)) {
+                    data.put(o, null);
+                }
+                System.out.println(data);
+            }
+            System.out.println();
+        }
+
+
+
+    }
+
+
+
 }
