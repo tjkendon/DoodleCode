c0795673ac97f0c5c8705644c37c849a4ba51867
Tyson Kendon <tj@kendon.ca>
2022-05-27 14:45:03 -0600

    Moved PowerRank Comparator to it's own class

--- CompetitionRank/src/ca/kendon/competitionrank/OptionDataPowerRankComperator.java
+++ CompetitionRank/src/ca/kendon/competitionrank/OptionDataPowerRankComperator.java
@@ -1,2 +1,17 @@
-package ca.kendon.competitionrank;public class OptionDataPowerRankComperator {
+package ca.kendon.competitionrank;
+
+import java.util.Comparator;
+
+public class OptionDataPowerRankComperator implements Comparator<OptionData> {
+
+    private Option defaultOption;
+
+    public OptionDataPowerRankComperator (Option defaultOption) {
+        this.defaultOption = defaultOption;
+    }
+
+    @Override
+    public int compare(OptionData o1, OptionData o2) {
+        return Double.compare(o2.getPowerRank(defaultOption), o1.getPowerRank(defaultOption));
+    }
 }
