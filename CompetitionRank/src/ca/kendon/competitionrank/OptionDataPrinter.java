289f3b6b0527550a2932c7dd86a14236879bc41b
Tyson Kendon <tj@kendon.ca>
2022-06-07 14:54:18 -0600

    Added csv printer to view data more easily

--- CompetitionRank/src/ca/kendon/competitionrank/OptionDataPrinter.java
+++ CompetitionRank/src/ca/kendon/competitionrank/OptionDataPrinter.java
@@ -1,11 +1,10 @@
 package ca.kendon.competitionrank;
 
 import java.util.Map;
-import java.util.Set;
 
 public class OptionDataPrinter {
 
-    public static String printToString(OptionData data, Option defaultOption) {
+    public static String fullDataPretty(OptionData data, Option defaultOption) {
         String returnString = String.format("%s%n\tCount: %s%n\tAverage Votes: %.3f%n\tAverage Rank: %.3f" +
                         "%n\tPower Rank: %.3f%n\tPercent Over Random: %.0f%%%n\tWin Rate: %.3f",
                 data.getOption().getName(),
@@ -26,5 +25,26 @@ public class OptionDataPrinter {
         }
         return returnString;
 
+    }
+
+    public static String fullDataCSV(OptionData data, Option defaultOption) {
+        String returnString = String.format("%s,%s,%f,%f,%f,%f,%f",
+                data.getOption().getName(),
+                data.getCount(),
+                data.getAverageVotes(),
+                data.getAverageRank(),
+                data.getPowerRank(defaultOption),
+                data.getBetterRate(defaultOption) * 100,
+                data.getOverallWinRate()
+        );
+        returnString += String.format(",%s,", data.getWonAgainst().size());
+        for( Map.Entry<Option, Integer> e: data.getWonAgainst().entrySet()) {
+            returnString += " " + e.getKey() + ":" + e.getValue();
+        }
+        returnString += String.format(",%s,", data.getLostTo().size());
+        for( Map.Entry<Option, Integer> e: data.getLostTo().entrySet()) {
+            returnString += " " + e.getKey() + ":" + e.getValue();
+        }
+        return returnString;
     }
 }
