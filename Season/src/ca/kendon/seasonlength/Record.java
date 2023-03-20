a31d926059c90adb66fb1ced0a06e88a5e45915c
Tyson Kendon <tj@kendon.ca>
2023-03-19 18:39:57 -0600

    Adding data for Record

--- Season/src/ca/kendon/seasonlength/Record.java
+++ Season/src/ca/kendon/seasonlength/Record.java
@@ -1,4 +1,61 @@
 package ca.kendon.seasonlength;
 
+
+
 public class Record {
+
+    private Competitor competitor;
+    int wins;
+    int overtimeWins;
+    int losses;
+    int overtimeLosses;
+    int ties;
+
+    public Record(Competitor competitor) {
+        this.competitor = competitor;
+    }
+
+    public void addWin() {
+        wins++;
+    }
+
+    public void addOvertimeWin() {
+        overtimeWins++;
+    }
+
+    public void addTie() {
+        ties++;
+    }
+
+    public void addOvertimeLoss() {
+        overtimeLosses++;
+    }
+
+    public void addLoss() {
+        losses++;
+    }
+
+    public Competitor getCompetitor() {
+        return competitor;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public int getOvertimeWins() {
+        return overtimeWins;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public int getOvertimeLosses() {
+        return overtimeLosses;
+    }
+
+    public int getTies() {
+        return ties;
+    }
 }
