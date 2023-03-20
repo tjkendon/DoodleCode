80b74aac92eb9bae7e609062b4838275fe1e856b
Tyson Kendon <tj@kendon.ca>
2023-03-19 18:13:17 -0600

    Clean up

--- SeasonLength/Competator.java
+++ SeasonLength/Competator.java
@@ -3,5 +3,16 @@ public class Competator {
     private String name;
     private double trueStrength;
 
+    public Competator(String name, double strength) {
+        this.name = name;
+        this.trueStrength = strength;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public double getTrueStrength() {
+        return trueStrength;
+    }
 }
