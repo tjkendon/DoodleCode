f9c7fced23e7bf29c47eea1294749328ccbe3610
Tyson Kendon <tj@kendon.ca>
2023-03-19 18:47:08 -0600

    Added model to handle competition algorithm

---
+++ Season/src/ca/kendon/seasonlength/CompetitionModel.java
@@ -0,0 +1,7 @@
+package ca.kendon.seasonlength;
+
+import java.util.List;
+
+public interface CompetitionModel {
+    List<Record> compete();
+}
--- Season/src/ca/kendon/seasonlength/Season.java
+++ Season/src/ca/kendon/seasonlength/Season.java
@@ -1,2 +1,30 @@
-package ca.kendon.seasonlength;public class Season {
+package ca.kendon.seasonlength;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public abstract class Season {
+
+    private List<Competitor> competitors;
+    private List<Record> results;
+
+    private CompetitionModel model;
+
+    public Season(List<Competitor> competitors) {
+        this.competitors = competitors;
+    }
+
+    public List<Competitor> getCompetitors() {
+        return competitors;
+    }
+
+    public List<Record> getResults() {
+        return results;
+    }
+
+    public void compete() {
+        results = model.compete();
+    }
+
+
 }
