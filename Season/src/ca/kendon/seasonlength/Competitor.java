5441a9344c7e81fe156dfd52384f5887edfb3d5e
Tyson Kendon <tj@kendon.ca>
2023-03-19 18:20:22 -0600

    Starting Competitor class.

--- Season/src/ca/kendon/seasonlength/Competitor.java
+++ Season/src/ca/kendon/seasonlength/Competitor.java
@@ -1,4 +1,20 @@
 package ca.kendon.seasonlength;
 
 public class Competitor {
+
+    private String name;
+    private double trueStrength;
+
+    public Competitor(String name, double trueStrength) {
+        this.name = name;
+        this.trueStrength = trueStrength;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getTrueStrength() {
+        return trueStrength;
+    }
 }
